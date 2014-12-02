ff7f009e504d66ba99de3b0b5eba84361cf781c2
Chris LaVoie <clavoie@gmail.com>
2014-12-02 16:54:29 -0500

    bumping rev number to 1.2.0

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject charset "1.1.0-SNAPSHOT"
+(defproject charset "1.2.0-SNAPSHOT"
   :description "Clojure wrappers around java.nio.charset.Charset"
   :url "https://github.com/clavoie/charset.git"
   :license {:name "Eclipse Public License"
