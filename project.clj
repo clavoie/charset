2d6a43637016fbe5654bd6fb2be456d7ade02413
Chris LaVoie <clavoie@gmail.com>
2014-12-02 16:50:49 -0500

    bumped the clojure number to 1.6.0. fixed the warn-on-reflection errors which were happening

--- project.clj
+++ project.clj
@@ -3,7 +3,7 @@
   :url "https://github.com/clavoie/charset.git"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.6.0"]]
   :global-vars {*warn-on-reflection* true}
   :plugins [[codox "0.6.6"]]
   :codox {:src-dir-uri "http://github.com/clavoie/charset/blob/master/"
--- src/charset/bytes.clj
+++ src/charset/bytes.clj
@@ -1,26 +1,41 @@
 (ns charset.bytes
   "Functions used to get the bytes of a value in a particular charset"
-  (:require [charset.core :refer [ascii utf-8 utf-16]]))
+  (:require [charset.core :refer [ascii utf-8 utf-16]])
+  (:import [java.nio.charset Charset]))
 
 (defn get-bytes
-  "Calls .getBytes() on value. The charset argument can be a
-   java.nio.charset.Charset object or the String name of a charset,
-   such as \"utf-8\". If no charset is specified the default
-   charset is used. Returns byte[]"
-  ([value] (.getBytes value))
-  ([value charset] (.getBytes value charset)))
+  "Calls .getBytes() on value. If no charset is specified the default charset
+  is used. Returns byte[]
+
+  value   - (String) the value to get he bytes of
+  charset - (String/Charset) either the name of a Charset or an instance
+            of the Charset class. Throws an IllegalArgumentException if charset
+            is neither.
+  "
+  ([^String value] (.getBytes value))
+  ([^String value charset]
+   (cond
+    (string? charset) (.getBytes value (str charset))
+    (instance? Charset charset) (.getBytes value ^Charset charset)
+    true (throw (IllegalArgumentException. "The charset parameter must be either a String or an instance of the Charset class")))))
 
 (defn ascii-bytes
-  "Returns value encoded as an ascii byte[]"
+  "Returns value encoded as an ascii byte[]
+
+  value - the value to get the bytes of"
   [value]
   (get-bytes value ascii))
 
 (defn utf8-bytes
-  "Returns value encoded as a utf-8 byte[]"
+  "Returns value encoded as a utf-8 byte[]
+
+  value - the value to get the bytes of"
   [value]
   (get-bytes value utf-8))
 
 (defn utf16-bytes
-  "Returns value encoded as a utf-16 byte[]"
+  "Returns value encoded as a utf-16 byte[]
+
+  value - the value to get the bytes of"
   [value]
   (get-bytes value utf-16))
